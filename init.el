464e35760fa9d4a33e1d4e682f268111c15da814
Gilbert <gilbertwong96@icloud.com>
2022-07-02 16:50:10 +0800

    Add docker and javascript layer

--- init.el
+++ init.el
@@ -32,7 +32,8 @@ This function should only modify configuration layer settings."
 
    ;; List of configuration layers to load.
    dotspacemacs-configuration-layers
-   '(sql
+   '(javascript
+     sql
      ;; ----------------------------------------------------------------
      ;; Example of useful layers you may want to use right away.
      ;; Uncomment some layer names and press `SPC f e R' (Vim style) or
@@ -73,6 +74,7 @@ This function should only modify configuration layer settings."
      erlang
      elixir
      haskell
+     docker
      python
      rust
      go
